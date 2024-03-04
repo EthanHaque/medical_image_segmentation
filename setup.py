07ae26c1e2294b83eb27e0d3b70199fe0771939d
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-04 09:35:04 -0500

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='medical_image_segmentation',
+    version='0.1.0',
+    license='MIT',
+    description='',
+    author='Ethan Haque',
+    author_email='ethanhaque@princeton.edu',
+    url='https://github.com/EthanHaque/medical-image-segmentation',
+    install_requires=[
+    ],
+    classifiers=[
+    ],
+)
