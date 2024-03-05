e8bcb631e5d1b38e10ca4c1aee48f96f4d97787e
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-04 23:36:39 -0500

    Plotting image shapes

--- medical_image_segmentation/visualize/viz.py
+++ medical_image_segmentation/visualize/viz.py
@@ -1,12 +1,21 @@
+from typing import List
+
 import medical_image_segmentation.analyze_data.utils as utils
 import matplotlib.pyplot as plt
-
 import numpy as np
 
-dimensions = get_dicom_image_dimensions(files, num_threads=4)
+
+def plot_image_shapes(image_shapes: List[List[int]]):
+    """
+    Plot image sizes on a scatter plot.
+
+    Parameters
+    ----------
+    image_shapes : List[int] List of width, height pairs.
+    """
     widths = []
     heights = []
-    for file, dims in dimensions.items():
+    for dims in image_shapes:
         widths.append(dims[0])
         heights.append(dims[1])
 
@@ -21,5 +30,11 @@ dimensions = get_dicom_image_dimensions(files, num_threads=4)
     plt.ylabel("Height (pixels)")
     plt.tight_layout()
 
-    # Show the plot
     plt.show()
+
+
+if __name__ == "__main__":
+    dir_path = ["/scratch/gpfs/eh0560/data/med_datasets", "/scratch/gpfs/RUSTOW/med_datasets"]
+    files = utils.get_file_paths(dir_path, lambda path: path.endswith(".dcm"))[:10000]
+    dimensions = utils.get_dicom_image_dimensions(files, num_threads=4)
+    plot_image_shapes(list(dimensions.values()))
