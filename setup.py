5709cb524a178e1722d6998972ae022b1002ac93
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-05 19:02:51 -0500

    Making package installable

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='medical_image_segmentation',
     version='0.1.0',
     license='MIT',
+    packages=find_packages(),
     description='',
     author='Ethan Haque',
     author_email='ethanhaque@princeton.edu',
     url='https://github.com/EthanHaque/medical-image-segmentation',
     install_requires=[
+        "pydicom",
+        "numpy"
     ],
     classifiers=[
     ],
