c78a086bd3ecd8b06a9efc716627557d0f68e73f
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-08 19:56:55 -0500

    Writing subset images

--- medical_image_segmentation/analyze_data/create_subset.py
+++ medical_image_segmentation/analyze_data/create_subset.py
@@ -1,7 +1,12 @@
+import hashlib
 import json
 import os
 import random
 from typing import List
+
+from PIL import Image
+import numpy as np
+import pydicom
 
 import medical_image_segmentation.analyze_data.utils as utils
 
@@ -31,9 +36,10 @@ def get_subset_dicom_image_paths() -> List[str]:
             json.dump(dimensions, f)
 
     dimensions_without_outliers = {}
-    threshold = 768
+    upper_bound = 768
+    lower_bound = 256
     for path, dim in dimensions.items():
-        if dim[0] <= threshold and dim[1] <= threshold:
+        if lower_bound <= dim[0] <= upper_bound and lower_bound <= dim[1] <= upper_bound:
             dimensions_without_outliers[path] = dim
 
     # Map the datasets to image paths.
@@ -68,9 +74,85 @@ def get_subset_dicom_image_paths() -> List[str]:
     return sorted(subset_file_paths)
 
 
+def write_raw_image_subset(image_paths: List[str], output_dir: str, num_processes: int =1) -> int:
+    """
+    Reads DICOM images from the image_paths and writes them as raw images to output_dir.
+
+    Parameters
+    ----------
+    output_dir : str The path to the output directory to save the raw images.
+    image_paths : List[str] A list of file paths to DICOM images.
+    num_processes : int The number of cores to batch tasks on.
+
+    Returns
+    -------
+    int
+        The number of images successfully written.
+    """
+    if len(image_paths) != len(set(image_paths)):
+        raise ValueError("Duplicate paths contained in image_paths.")
+    os.makedirs(output_dir, exist_ok=True)
+
+    statuses = utils.process_dicom_files(image_paths, write_raw_image_subset_helper, output_dir=output_dir, num_processes=num_processes)
+
+    count = 0
+    for path, status in statuses.items():
+        if status["error"] is None:
+            count += 1
+        else:
+            print(status)
+
+    return count
+
+
+def write_raw_image_subset_helper(image_path: str, *args, **kwargs) -> dict:
+    """
+    Helper function to write an individual DICOM image to output dir.
+
+    Parameters
+    ----------
+    image_path: str The path to the DICOM image to write. The name of the writen file will
+    be the hash of the DICOM image.
+
+    Returns
+    -------
+    dict
+        A dictionary with three keys:
+        - image_path The path to the source DICOM image.
+        - output_path The path to the destination image. Is None if the image is invalid.
+        - error An error message if the image is invalid. Is None if no errors occurred.
+    """
+    arr = pydicom.dcmread(image_path).pixel_array
+    arr.flags.writeable = False
+    sha_hash = hashlib.sha256(arr).hexdigest()
+    output_path = os.path.join(kwargs["output_dir"], f"{sha_hash}.png")
+
+    try:
+        min_val = np.nanmin(arr)
+        max_val = np.nanmax(arr)
+        if max_val > min_val:
+            arr = ((arr - min_val) / (max_val - min_val) * 65535).astype(np.uint16)
+        else:
+            raise ValueError(f"Invalid image data for image {image_path}")
+    except ValueError as e:
+        return {"image_path": image_path, "output_path": None, "error": e}
+
+    try:
+        image = Image.fromarray(arr)
+        image.save(output_path, format='PNG', bits=16)
+        return {"image_path": image_path, "output_path": output_path, "error": None}
+    except Exception as e:
+        return {"image_path": image_path, "output_path": output_path, "error": e}
+
+
 if __name__ == "__main__":
-    subset_output_path = "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/dicom_image_analysis_info/image_path_list"
-    image_subset = get_subset_dicom_image_paths()
-    with open(subset_output_path, "w") as f:
-        for image_path in image_subset:
-            f.write(image_path + "\n")
+    subset_path = "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/dicom_image_analysis_info/image_path_list"
+    with open(subset_path, "r") as f:
+        paths = f.read().splitlines()
+        paths = [path.strip() for path in paths]
+
+    write_path = "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/test"
+
+    random.shuffle(paths)
+    count = write_raw_image_subset(paths[:1000], write_path, num_processes=4)
+    print(count)
