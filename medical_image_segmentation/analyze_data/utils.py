03b183231bed34034be974baef105077f01f7fd0
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-12 22:35:13 -0400

    Renaming and getting rid of unused code

--- medical_image_segmentation/analyze_data/utils.py
+++ medical_image_segmentation/analyze_data/utils.py
@@ -3,6 +3,8 @@ from functools import partial
 from typing import List, Union, Callable, Tuple
 import os
 
+from PIL import Image
+
 import pydicom
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
@@ -58,17 +60,18 @@ def get_file_type_counts(roots: Union[str, List[str]]) -> dict[str, int]:
     return dict(Counter(extensions))
 
 
-def process_dicom_files(image_paths: List[str], processing_function: Callable[[str, ...], dict], num_processes: int = 1,
-                        *args, **kwargs) -> \
-        dict[str, dict]:
+def process_files(image_paths: List[str],
+                  processing_function: Callable[[str, ...], dict],
+                  num_processes: int = 1,
+                  *args, **kwargs) -> dict[str, dict]:
     """
-    Processes DICOM files using the given processing function and returns the results as a dictionary where
-    each key is a file path and each value is some information about the DICOM file.
+    Processes files using the given processing function and returns the results as a dictionary where
+    each key is a file path and each value is some information about the file.
 
     Parameters
     ----------
-    image_paths : List[str] The paths of the DICOM files to process.
-    processing_function : Callable[[str], dict] The processing function to apply to every DICOM file path.
+    image_paths : List[str] The paths of the files to process.
+    processing_function : Callable[[str], dict] The processing function to apply to every file path.
     num_processes : int, optional [default = 1]: The number of processes to split the tasks among.
 
     Returns
@@ -80,7 +83,7 @@ def process_dicom_files(image_paths: List[str], processing_function: Callable[[s
     if num_processes < 1:
         raise ValueError(f"num_processes must be greater than 1, but got {num_processes}")
 
-    dicom_image_info = {}
+    image_info = {}
     with ProcessPoolExecutor(max_workers=num_processes) as executor:
         partial_processing_function = partial(processing_function, *args, **kwargs)
         future_to_file = {}
@@ -91,7 +94,7 @@ def process_dicom_files(image_paths: List[str], processing_function: Callable[[s
                 TimeElapsedColumn(),
                 TimeRemainingColumn(),
         ) as progress:
-            task = progress.add_task("Batching DICOM files...", total=len(image_paths), file_count=0)
+            task = progress.add_task("Batching files...", total=len(image_paths), file_count=0)
             for i, file_path in enumerate(image_paths):
                 future_to_file[executor.submit(partial_processing_function, file_path)] = file_path
                 progress.update(task, advance=1)
@@ -103,17 +106,17 @@ def process_dicom_files(image_paths: List[str], processing_function: Callable[[s
                 TimeElapsedColumn(),
                 TimeRemainingColumn(),
         ) as progress:
-            task = progress.add_task("Processing DICOM files...", total=len(image_paths), file_count=0)
+            task = progress.add_task("Processing files...", total=len(image_paths), file_count=0)
             for future in as_completed(future_to_file):
                 file_path = future_to_file[future]
                 try:
                     result = future.result()
                 except Exception:
                     raise RuntimeError(f"Error occurred during processing of {file_path}")
-                dicom_image_info[file_path] = result
+                image_info[file_path] = result
                 progress.update(task, advance=1)
 
-    return dicom_image_info
+    return image_info
 
 
 def get_dicom_image_dimensions(image_paths: List[str], num_processes: int = 1) -> dict[str, List[int]]:
@@ -131,7 +134,7 @@ def get_dicom_image_dimensions(image_paths: List[str], num_processes: int = 1) -
         A dictionary where the keys are the file paths and the values are a list where the first
         element is the width and the second is the height of the DICOM image.
     """
-    dimension_information = process_dicom_files(image_paths, _get_dicom_image_dimensions_helper, num_processes)
+    dimension_information = process_files(image_paths, _get_dicom_image_dimensions_helper, num_processes)
     value_as_list = {}
     for key, value in dimension_information.items():
         if not value:
@@ -161,9 +164,9 @@ def _get_dicom_image_dimensions_helper(image_path: str) -> dict:
         return {}
 
 
-def get_dicom_image_mean_and_std(image_paths: List[str], num_processes: int = 1) -> Tuple[float, float]:
+def get_raster_image_dimensions(image_paths: List[str], num_processes: int = 1) -> dict[str, List[int]]:
     """
-    Gets the mean and standard deviation over all images in the given list of image paths.
+    Gets the width and height of every image file in the given list of image paths.
 
     Parameters
     ----------
@@ -172,15 +175,23 @@ def get_dicom_image_mean_and_std(image_paths: List[str], num_processes: int = 1)
 
     Returns
     -------
-    Tuple[float, float]
-        The mean and standard deviation over all images in the given list of image paths.
+    dict[str, List[int, int]]
+        A dictionary where the keys are the file paths and the values are a list where the first
+        element is the width and the second is the height of the DICOM image.
     """
-    dimension_information = process_dicom_files(image_paths, _get_dicom_image_mean_and_std_helper, num_processes)
+    dimension_information = process_files(image_paths, _get_raster_image_dimensions_helper, num_processes)
+    value_as_list = {}
+    for key, value in dimension_information.items():
+        if not value:
+            continue
+        value_as_list[key] = [value["width"], value["height"]]
+
+    return value_as_list
 
 
-def _get_dicom_image_mean_and_std_helper(image_path: str) -> dict:
+def _get_raster_image_dimensions_helper(image_path: str) -> dict:
     """
-    Helper processing function to get DICOM image mean and standard deviation.
+    Helper processing function to get raster image dimensions.
 
     Parameters
     ----------
@@ -189,10 +200,11 @@ def _get_dicom_image_mean_and_std_helper(image_path: str) -> dict:
     Returns
     -------
     dict
-        A dictionary with two entries. One gives the mean of the pixels in the image and the other gives the
-        standard deviation of the pixels in the image.
+        A dictionary with two entries. One gives the image width and the other gives the image height.
     """
-    image_info = pydicom.dcmread(image_path, stop_before_pixels=False)
+    im = Image.open(image_path)
+    width, height = im.size
+    return {"width": width, "height": height}
 
 
 if __name__ == "__main__":
