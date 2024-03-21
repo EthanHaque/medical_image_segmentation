4eab02b4df817f7aafabd2c4747df493964784c9
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-21 09:50:12 -0400

    switching order of jitter

--- medical_image_segmentation/train/data_loaders/ffcv_loader.py
+++ medical_image_segmentation/train/data_loaders/ffcv_loader.py
@@ -25,25 +25,25 @@ def create_train_loader_ssl(this_device: str, beton_file_path: str, batch_size: 
     image_pipeline_1 = [
         ffcv.fields.rgb_image.RandomResizedCropRGBImageDecoder((image_size, image_size)),
         ffcv.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.ColorJitter(0.4, 0.4, 0.2, 0.1),
         ffcv.transforms.RandomGrayscale(0.2),
         ffcv.transforms.ToTensor(),
         ffcv.transforms.ToDevice(this_device, non_blocking=True),
         ffcv.transforms.ToTorchImage(),
         ffcv.transforms.NormalizeImage(imagenet_mean, imagenet_std, np.float32),
+        torchvision.transforms.ColorJitter(0.4, 0.4, 0.2, 0.1),
         torchvision.transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 2)),
     ]
 
     image_pipeline_2 = [
         ffcv.fields.rgb_image.RandomResizedCropRGBImageDecoder((image_size, image_size)),
         ffcv.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.ColorJitter(0.4, 0.4, 0.2, 0.1),
         ffcv.transforms.RandomGrayscale(0.2),
         ffcv.transforms.RandomSolarization(0.2, 128),
         ffcv.transforms.ToTensor(),
         ffcv.transforms.ToDevice(this_device, non_blocking=True),
         ffcv.transforms.ToTorchImage(),
-        ffcv.transforms.NormalizeImage(imagenet_mean, imagenet_std, np.float32)
+        ffcv.transforms.NormalizeImage(imagenet_mean, imagenet_std, np.float32),
+        torchvision.transforms.ColorJitter(0.4, 0.4, 0.2, 0.1),
     ]
 
     label_pipeline = [
