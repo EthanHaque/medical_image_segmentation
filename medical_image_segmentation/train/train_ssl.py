355a91cb1a64ca915a55c898397a6639387d79dc
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-23 01:43:17 -0400

    Updating to use lars

--- medical_image_segmentation/train/train_ssl.py
+++ medical_image_segmentation/train/train_ssl.py
@@ -16,18 +16,21 @@ from tqdm import tqdm
 
 from medical_image_segmentation.train.callback.linear_eval import SSLLinearEval
 from medical_image_segmentation.train.data_loaders.ffcv_loader import create_train_loader_ssl, create_val_loader_ssl
+from medical_image_segmentation.train.optimizer.lars import LARS
+from medical_image_segmentation.train.scheduler.cosine_annealing import LinearWarmupCosineAnnealingLR
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Self-Supervised Learning with BYOL and PyTorch")
     parser.add_argument("--epochs", type=int, default=300, help="Number of training epochs")
-    parser.add_argument("--lr", type=float, default=3e-4, help="Learning rate")
+    parser.add_argument("--lr", type=float, default=1.0, help="Learning rate")
+    parser.add_argument("--min_lr", type=float, default=1e-3, help="Minimum learning rate")
     parser.add_argument("--batch_size", type=int, default=2048, help="Batch size")
     parser.add_argument("--image_size", type=int, default=256, help="Image size")
     parser.add_argument("--num_workers", type=int, default=int(os.environ.get("SLURM_CPUS_PER_TASK", "16")), help="Number of workers for data loading")
     parser.add_argument("--num_gpus", type=int, default=int(os.environ.get("SLURM_GPUS_ON_NODE", "4")), help="Number of GPUs for training")
     parser.add_argument("--checkpoint_path", type=str, help="Path to checkpoint file to restore training")
     parser.add_argument("--warmup_epochs", type=int, default=10, help="Number of epochs to warm up to set learning rate")
-    parser.add_argument("--optimizer", type=str, default="adam", help="Optimizer to use")
+    parser.add_argument("--optimizer", type=str, default="lars", help="Optimizer to use")
     parser.add_argument("--dry", action="store_true", help="Dry run")
     parser.add_argument("--float_matmul_precision", type=str, default="highest", help="Setting float32 matrix multiplication precision. See https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html#torch.set_float32_matmul_preci")
     parser.add_argument("--train_subset_size", type=int, required=False)
@@ -72,12 +75,18 @@ class SelfSupervisedLearner(pl.LightningModule):
             optimizer = torch.optim.Adam(self.parameters(), lr=args.lr)
         elif args.optimizer == "sgd":
             optimizer = torch.optim.SGD(self.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-6)
+        elif args.optimizer == "lars":
+            optimizer = LARS(self.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-6)
         else:
             raise NotImplementedError(f"Optimizer {self.optimizer} not implemented as option")
 
-        warmup_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.1, total_iters=args.warmup_epochs)
-        cosine_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs - args.warmup_epochs)
-        scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, schedulers=[warmup_scheduler, cosine_scheduler], milestones=[args.warmup_epochs])
+        scheduler = LinearWarmupCosineAnnealingLR(
+            optimizer,
+            warmup_epochs=args.warmup_epochs,
+            max_epochs=args.epochs,
+            warmup_start_lr=args.min_lr,
+            eta_min=args.min_lr
+        )
 
         return [optimizer], [scheduler]
 
