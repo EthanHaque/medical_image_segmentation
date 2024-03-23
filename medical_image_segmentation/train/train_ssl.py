2af4ab898219504ace15992451eca195e1990fcf
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-23 00:51:09 -0400

    Adding linear eval back in

--- medical_image_segmentation/train/train_ssl.py
+++ medical_image_segmentation/train/train_ssl.py
@@ -143,7 +143,7 @@ def setup_train_objects():
     logger = pl.loggers.CSVLogger("logs")
     callbacks = [
         pl.callbacks.LearningRateMonitor(logging_interval="epoch", log_momentum=True, log_weight_decay=True),
-        # SSLLinearEval(256, drop_p=0.2, num_classes=1000),
+        SSLLinearEval(256, drop_p=0.2, num_classes=1000),
     ]
 
     trainer = pl.Trainer(
