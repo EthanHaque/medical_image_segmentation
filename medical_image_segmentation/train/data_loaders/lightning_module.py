7efaa83c39a62f5da39506216919fc352334da4a
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-03-25 05:34:16 -0400

    Only decoding

--- medical_image_segmentation/train/data_loaders/lightning_module.py
+++ medical_image_segmentation/train/data_loaders/lightning_module.py
@@ -66,15 +66,15 @@ class CIFAR100FFCVDataModule(LightningDataModule):
 
     def train_dataloader(self):
         train_transforms = [
-                transform_lib.RandomResizedCrop(32),
-                transform_lib.RandomHorizontalFlip(),
-                # transform_lib.RandomApply([transform_lib.ColorJitter(0.4, 0.4, 0.2, 0.1)], p=0.8),
-                transform_lib.RandomGrayscale(p=0.2),
-                # transform_lib.RandomApply([transform_lib.GaussianBlur(kernel_size=23)], p=1.0),
-                transform_lib.RandomSolarize(128, p=0.0),
-                transform_lib.ToImage(),
-                transform_lib.ToDtype(torch.float32, scale=True),
-                transform_lib.Normalize(mean=self.mean, std=self.std)
+                # transform_lib.RandomResizedCrop(32),
+                # transform_lib.RandomHorizontalFlip(),
+                # # transform_lib.RandomApply([transform_lib.ColorJitter(0.4, 0.4, 0.2, 0.1)], p=0.8),
+                # transform_lib.RandomGrayscale(p=0.2),
+                # # transform_lib.RandomApply([transform_lib.GaussianBlur(kernel_size=23)], p=1.0),
+                # transform_lib.RandomSolarize(128, p=0.0),
+                # transform_lib.ToImage(),
+                # transform_lib.ToDtype(torch.float32, scale=True),
+                # transform_lib.Normalize(mean=self.mean, std=self.std)
         ]
   
         image_pipeline = [ SimpleRGBImageDecoder()] + train_transforms
