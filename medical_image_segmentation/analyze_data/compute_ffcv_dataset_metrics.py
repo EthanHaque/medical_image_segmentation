e7ec02a6c1723d5897c61143e9b6e92c5eced31b
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-01 00:58:33 -0400

    More print statements

--- medical_image_segmentation/analyze_data/compute_ffcv_dataset_metrics.py
+++ medical_image_segmentation/analyze_data/compute_ffcv_dataset_metrics.py
@@ -49,6 +49,7 @@ def compute_mean_and_std(beton_file_path: str, epsilon=1e-8):
     epsilon_tensor = torch.tensor(epsilon)
     print(sum_squared)
     print(sum_squared / num_pixels)
+    print(mean ** 2)
     print(sum_squared / num_pixels - mean ** 2)
     std = torch.sqrt((sum_squared / num_pixels - mean ** 2) + epsilon_tensor)
 
