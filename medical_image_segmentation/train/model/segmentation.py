eab459e9462915bcb37f97bb58dc10f54d1093f6
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-05 15:18:32 -0400

    Decreasing number of classes

--- medical_image_segmentation/train/model/segmentation.py
+++ medical_image_segmentation/train/model/segmentation.py
@@ -14,13 +14,12 @@ class Segmentation(pl.LightningModule):
             encoder_name=self.hparams.arch,  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
             encoder_weights=None,  # use `imagenet` pre-trained weights for encoder initialization
             in_channels=1,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-            classes=2,  # model output channels (number of classes in your dataset)
+            classes=1,  # model output channels (number of classes in your dataset)
         )
         
         self.loss_fn = nn.BCEWithLogitsLoss()
 
     def forward(self, x) -> torch.Tensor:
-        print(x.shape)
         return self.model(x)
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
