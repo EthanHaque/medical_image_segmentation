67e880d161597b334a12a3873024535ffa8515f8
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-08 07:21:40 -0400

    Adding print

--- medical_image_segmentation/train/model/byol_pytorch.py
+++ medical_image_segmentation/train/model/byol_pytorch.py
@@ -257,6 +257,7 @@ class BYOL(pl.LightningModule):
         # Must put loaders in this method to ensure DDP process groups are constructed before creating data loaders
         device = self.trainer.local_rank
         distributed = len(self.trainer.device_ids) > 1
+        print(distributed)
         dataset = get_datamodule(self.hparams.dataset)
         module = dataset(
             batch_size=self.hparams.batch_size,
