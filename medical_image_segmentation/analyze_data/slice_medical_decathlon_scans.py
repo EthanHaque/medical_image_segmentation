c89e737d323292b38bcf651c16ce14ea6b006362
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-11 23:51:31 -0400

    Adding in basic read and get shape

--- medical_image_segmentation/analyze_data/slice_medical_decathlon_scans.py
+++ medical_image_segmentation/analyze_data/slice_medical_decathlon_scans.py
@@ -1,4 +1,3 @@
-import time
 from typing import Tuple, List
 import os
 import argparse
@@ -55,10 +54,8 @@ def get_slice_output_path(segmentation_file: NIBSegmentationFile, output_dir: st
 
 def save_nii_slices(segmentation_file: NIBSegmentationFile, output_dir: str, slice_dim: int):
     """Saves slices of nifti file to an output directory."""
-    num_slices = segmentation_file.get_shape()[slice_dim]
-    time.sleep(2)
-    # image_arr = segmentation_file.get_arr()
-    # num_slices = image_arr.shape[slice_dim]
+    image_arr = segmentation_file.get_arr()
+    num_slices = image_arr.shape[slice_dim]
 
     # for slice_number in range(num_slices):
     #     slice = image_arr.take(indices=slice_number, axis=slice_dim)
