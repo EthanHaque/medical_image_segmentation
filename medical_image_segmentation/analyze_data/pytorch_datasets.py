57d636eee7a2a5ba0733b668746c4764c38ee9ca
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-12 02:36:04 -0400

    No longer scaling mask

--- medical_image_segmentation/analyze_data/pytorch_datasets.py
+++ medical_image_segmentation/analyze_data/pytorch_datasets.py
@@ -265,7 +265,7 @@ class DecathlonDataset(Dataset):
         if self.mask_transform:
             mask = self.mask_transform(mask)
 
-        mask /= 255
+        # mask /= 255
 
         return image, mask
 
