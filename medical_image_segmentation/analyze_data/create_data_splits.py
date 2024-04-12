3e216df441709b8f81b5bb01bed9728d5d86cefc
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-11 21:41:55 -0400

    Using correct character for splitting

--- medical_image_segmentation/analyze_data/create_data_splits.py
+++ medical_image_segmentation/analyze_data/create_data_splits.py
@@ -43,7 +43,7 @@ def get_ids(image_dir):
     files = os.listdir(image_dir)
     ids = []
     for file in files:
-        image_id = file.split("_")[-1].split("0")[0]
+        image_id = file.split("_")[-1].split(".")[0]
         image_id = int(image_id)
         ids.append(image_id)
     return ids
