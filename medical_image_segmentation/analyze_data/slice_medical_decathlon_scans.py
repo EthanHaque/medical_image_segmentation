623a00578fa6868767e7ca3a8b7cc3d5728028fe
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-11 23:36:14 -0400

    Testing speed

--- medical_image_segmentation/analyze_data/slice_medical_decathlon_scans.py
+++ medical_image_segmentation/analyze_data/slice_medical_decathlon_scans.py
@@ -54,11 +54,12 @@ def get_slice_output_path(segmentation_file: NIBSegmentationFile, output_dir: st
 
 def save_nii_slices(segmentation_file: NIBSegmentationFile, output_dir: str, slice_dim: int):
     """Saves slices of nifti file to an output directory."""
-    image_arr = segmentation_file.get_arr()
-    num_slices = image_arr.shape[slice_dim]
+    num_slices = segmentation_file.get_shape()[slice_dim]
+    # image_arr = segmentation_file.get_arr()
+    # num_slices = image_arr.shape[slice_dim]
 
-    for slice_number in range(num_slices):
-        slice = image_arr.take(indices=slice_number, axis=slice_dim)
+    # for slice_number in range(num_slices):
+    #     slice = image_arr.take(indices=slice_number, axis=slice_dim)
         # if not is_mask:
         #     slice = ((slice - slice.min()) / (slice.max() - slice.min()) * 255)
         # else:
