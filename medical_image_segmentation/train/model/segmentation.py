Fixing argmax after softmax to just threshold

medical_image_segmentation/train/model/segmentation.py
@@ -59,7 +59,6 @@ class Segmentation(pl.LightningModule):
         images, masks = batch
 
         logits = self.forward(images)
-        print(f"val logits max: {logits.max().item()}")
         loss = self.loss(logits, masks)
 
         metric_log = {
@@ -80,7 +79,5 @@ class Segmentation(pl.LightningModule):
     def predict_step(self, batch, batch_idx):
         images, masks = batch
         logits = self(images)
-        print(f"pred {logits.max().item()}")
-        predicted_masks = torch.argmax(torch.softmax(logits, dim=1), dim=1)
-        print(f"pred processed {predicted_masks.max().item()}")
+        predicted_masks = (logits > 0.5).float()
         return images, predicted_masks, masks