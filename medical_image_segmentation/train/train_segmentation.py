cccba1bdd1aca3e6be128142ea2b6093ae875020
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-12 03:00:34 -0400

    Show scaled masks

--- medical_image_segmentation/train/train_segmentation.py
+++ medical_image_segmentation/train/train_segmentation.py
@@ -65,9 +65,9 @@ def main(args):
 
     preds = trainer.predict(model, decathlon_dataset)
     images, pred_masks, true_masks = preds[0]
-    pred_masks *= 255
-    save_combined_image_grid(images, pred_masks, true_masks, "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/images", grid_size=16)
-    save_image_grid(pred_masks.unsqueeze(1), "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/images", grid_size=16)
+    pred_masks_scaled = (pred_masks * 255).byte()
+    save_combined_image_grid(images, pred_masks_scaled, true_masks, "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/images", grid_size=16)
+    # save_image_grid(pred_masks.unsqueeze(1), "/scratch/gpfs/eh0560/repos/medical-image-segmentation/data/images", grid_size=16)
 
 if __name__ == "__main__":
     args = parse_args()
