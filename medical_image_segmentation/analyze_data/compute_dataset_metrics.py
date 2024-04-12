1d138b9b95a674ded1da94abaf6a27eba88c8a1d
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-12 10:05:01 -0400

    Getting ready to compute liver dataset stats

--- medical_image_segmentation/analyze_data/compute_dataset_metrics.py
+++ medical_image_segmentation/analyze_data/compute_dataset_metrics.py
@@ -62,7 +62,7 @@ def compute_mean_and_std_ffcv(beton_file_path, batch_size=8, num_workers=2):
 if __name__ == "__main__":
     images_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Liver/images"
     masks_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Liver/masks"
-    split_file = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Liver/"
+    split_file = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Liver/split_all_in_train.json"
     transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
     dataset = DecathlonDataset(images_dir, masks_dir, 1, transform, transform, "train", split_file)
     mean, std = compute_mean_and_std_pytorch(dataset)
