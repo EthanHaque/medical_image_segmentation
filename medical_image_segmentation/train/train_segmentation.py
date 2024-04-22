39df300a08b361fee49f3120ccf9ecf235193573
ethanhaque <43977335+EthanHaque@users.noreply.github.com>
2024-04-22 11:28:52 -0400

    Fixing task

--- medical_image_segmentation/train/train_segmentation.py
+++ medical_image_segmentation/train/train_segmentation.py
@@ -62,10 +62,10 @@ def main(args):
     )
 
     n_classes = 1
-    images_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Heart/images"
-    masks_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Heart/masks"
+    images_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task02_Heart/images"
+    masks_dir = "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task02_Heart/masks"
     split_file = (
-        "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task03_Heart/split_10_percent_train.json"
+        "/scratch/gpfs/RUSTOW/med_datasets/medicaldecathlon/sliced_data/Task02_Heart/split_10_percent_train.json"
     )
     decathlon_dataset = DecathlonLiverDataModule(
         images_dir,
